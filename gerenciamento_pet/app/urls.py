69091e150c6815639729b8b1a28bcf4a6cf64f6f
unknown <isaiasorton@gmail.com>
2021-02-06 00:49:55 -0300

    feito a listagem de clientes e listagem de um unico cliente por id, além de persnolaziar a page do cliente

--- gerenciamento_pet/app/urls.py
+++ gerenciamento_pet/app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import cliente_views
 
 urlpatterns = [
-    path('cadastrar_cliente', cliente_views.cadastrar_cliente, name='cadastrar_cliente')
+    path('cadastrar_cliente', cliente_views.cadastrar_cliente, name='cadastrar_cliente'),
+    path('listar_clientes', cliente_views.listar_clintes, name='listar_clientes'),
+    path('lista_cliente/<int:id>', cliente_views.listar_cliente_id, name='listar_cliente_id'),
 ]
